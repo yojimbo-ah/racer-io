a6e6b73137f882a55f7d3b28e7dac8371be82031
Abbad Ahmed <abbad.ahmed.gg@gmail.com>
2026-08-26 16:12:41 +0100

    fixed the databse setup , erro was in the configuration

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,7 @@ import { CheaterDetectedListener } from "./events/listeners/cheaterDetectedListe
 import UserCreatedSagaResultListener from "./events/listeners/userCreationResultListener";
 import blacklistRedis from "./blacklistRedis";
 import { startOutboxRelay } from "./outbox/outboxRelay";
+import { prepareMongo } from "./outbox/setMongoosePrimary";
 
 const connect = async () => {
     // making sure that the enviromental variables exist 
@@ -30,35 +31,35 @@ const connect = async () => {
         throw new Error('REDIS HOST BLACKLIST is not defined') ;
     }
 
-    try {
 
-        await mongoose.connect(process.env.MONGO_URI!) ;
-        await natsWrapper.connect(process.env.NATS_CLUSTER_ID , process.env.NATS_CLIENT_ID , {
-            url : process.env.NATS_URL
-        }) ;
-        // connect to the redis client all the services will connect to this
-        // database so we can check the blacklisted users with sharing state (single source of truth)
-        await blacklistRedis.connect() ;
-        natsWrapper.client.on('close' , () => {
-            console.log('NATS connection clossed') ;
-            process.exit() ;
-        })
+    await mongoose.connect(process.env.MONGO_URI!) ;
+    // configure the databse so we can handle the watch and changes in it 
+    // so we can detect when we should publish events
+    await prepareMongo() ;
+    console.log('connected to mongoose correctly') ;
+    await natsWrapper.connect(process.env.NATS_CLUSTER_ID , process.env.NATS_CLIENT_ID , {
+        url : process.env.NATS_URL
+    }) ;
+    // connect to the redis client all the services will connect to this
+    // database so we can check the blacklisted users with sharing state (single source of truth)
+    await blacklistRedis.connect() ;
+    natsWrapper.client.on('close' , () => {
+        console.log('NATS connection clossed') ;
+        process.exit() ;
+    })
 
-        process.on('SIGINT' , () => natsWrapper.client.close()) ;
-        process.on('SIGTERM' , () => natsWrapper.client.close()) ;
+    process.on('SIGINT' , () => natsWrapper.client.close()) ;
+    process.on('SIGTERM' , () => natsWrapper.client.close()) ;
 
-        new CheaterDetectedListener(natsWrapper.client).listen() ;
-        new UserCreatedSagaResultListener(natsWrapper.client).listen() ;
+    new CheaterDetectedListener(natsWrapper.client).listen() ;
+    new UserCreatedSagaResultListener(natsWrapper.client).listen() ;
 
-        // created the rely to resend the events that had been set in the databse 
-        // but didnt suscess to start the publisher
-        
-        await startOutboxRelay() ;
-        app.listen(3000 , () => {
-            console.log("listening  on 3000") ;
-        })
-    } catch (error) {
-        console.log(error)
-    }
+    // created the rely to resend the events that had been set in the databse 
+    // but didnt suscess to start the publisher
+    
+    await startOutboxRelay() ;
+    app.listen(3000 , () => {
+        console.log("listening  on 3000") ;
+    })
 }
 connect() ;
--- auth/src/outbox/outboxRelay.ts
+++ auth/src/outbox/outboxRelay.ts
@@ -8,6 +8,7 @@ import { natsWrapper } from "../nats-wrapper";
 // outbox-relay.ts
 export async function startOutboxRelay() {
     // catch up on anything missed while relay was down
+    console.log('starting the ralay function tracking') ;
     const pending = await OutboxEvent.find({ published: false }).sort({ createdAt: 1 });
     for (const doc of pending) {
         await publishAndMark(doc);
@@ -18,12 +19,18 @@ export async function startOutboxRelay() {
         const doc = await OutboxEvent.findById(change.documentKey._id);
         if (doc && !doc.published) await publishAndMark(doc);
     });
+    
+    changeStream.on('error', (err) => {
+        console.error('[outbox-relay] change stream error:', err);
+    });
+
+    console.log('[outbox-relay] relay is now watching for new events');
 }
 
 export async function publishAndMark(doc: OutboxEventDocument) {
     try {
         // check the event type then we publish depending on the event
-
+        console.log(doc) ;
         if (doc.eventType === Subjects.userCreated) {
             const payload = doc.payload as userCreatedEvent['data'] ;
             new UserCreatedPublisher(natsWrapper.client).publish(payload) ;
